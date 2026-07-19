c4ee5d57ec8b5ba5d2557c11420ccd0ae4ac6299
Bikrom2001 <bikromroy2001@gmail.com>
2026-07-19 16:51:19 +0600

    html Templates Setup

--- jobPortalProject/jobPortalProject/urls.py
+++ jobPortalProject/jobPortalProject/urls.py
@@ -1,4 +1,3 @@
-
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
@@ -6,4 +5,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
