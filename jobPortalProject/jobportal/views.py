22cab1be422026e1d4dbc12d5d65475c2244ccc6
Bikrom2001 <bikromroy2001@gmail.com>
2026-07-19 17:06:51 +0600

    User Registration Form html done

--- jobPortalProject/jobportal/views.py
+++ jobPortalProject/jobportal/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 def register_view(request):
     
-    return render(request, 'master/base-form.html')
+    context ={
+        'title':"Register Page",
+        'form_title': 'User Registration Form',
+        'form_btn': 'Register',
+    }
+    
+    return render(request, 'master/base-form.html', context)
